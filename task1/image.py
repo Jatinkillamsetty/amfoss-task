9cf5a4d38f6eee9f7108a12c6593a0ba54fd22cd
Jatinkillamsetty <jatinkillamsetty2006@gmail.com>
2025-02-10 18:31:04 +0530

    Delete task2 directory

--- task1/image.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image, ImageDraw, ImageFont
-img = Image.new('RGB', (200, 50), color=(255, 255, 255))
-
-d = ImageDraw.Draw(img)
-font = ImageFont.truetype("DejaVuSans-Bold.ttf", 40)  
-d.text((50, 5), "2 + 2", fill=(0, 0, 0), font=font)
-img.save("jatin.png")
